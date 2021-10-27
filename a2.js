4569ccbf36d2862c4de08a4ba0b88a60d35b2f78
mmll <xinyuelu2000@hotmail.com>
2021-10-27 17:08:20 -0400

    first commit

---
+++ a2.js
@@ -0,0 +1,40 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+      document.getElementById("navbar").style.backgroundColor = "white";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+      document.getElementById("navbar").style.backgroundColor = "white";
+  }
+    prevScrollpos = currentScrollPos;
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
